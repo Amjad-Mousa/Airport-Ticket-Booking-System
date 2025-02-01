a522dc8c1f3546dfa29907b9599b641cc6a98a08
amjad <amjad.mousa.600@gmail.com>
2025-02-01 20:42:47 +0200

    Modify TotalPrice Calculations

--- Airport Ticket Booking System/Services/FlightService.cs
+++ Airport Ticket Booking System/Services/FlightService.cs
@@ -3,7 +3,12 @@ using Airport_Ticket_Booking_System.DTOs;
 using Airport_Ticket_Booking_System.Entities;
 using Airport_Ticket_Booking_System.Helper;
 namespace Airport_Ticket_Booking_System.Services;
-
+using Airport_Ticket_Booking_System.Entities;
 public class FlightService
 {
+    public  static Flight? GetFlightById(string id)
+    {
+         return CsvHelperService.ReadFromCsv<Flight>("Flight.csv")
+            .FirstOrDefault(f=>f.Id==id);
+    }
 }
